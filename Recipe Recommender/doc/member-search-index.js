b205794de4158274a0b06032c75c0abf835ec6cc
Ayesha Ahmed <110387213+ayeshaahmd@users.noreply.github.com>
2024-12-02 22:37:48 +0500

    Add files via upload
    Java Documentation

---
+++ Recipe Recommender/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"RecipeRecommender","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"RecipeRecommender","l":"RecipeRecommender()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"RecipeRecommender","l":"showMainScreen()"},{"p":"<Unnamed>","c":"RecipeRecommender","l":"showUsernameScreen()"}]
